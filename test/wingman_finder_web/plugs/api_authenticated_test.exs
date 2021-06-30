34d08823163651e850d09c23522f24bdac2f21af
Robby Thompson <robsdudeson@gmail.com>
2021-06-30 16:54:59 -0400

    added cases for api_authenticated plug

--- test/wingman_finder_web/plugs/api_authenticated_test.exs
+++ test/wingman_finder_web/plugs/api_authenticated_test.exs
@@ -2,13 +2,29 @@ defmodule WingmanFinderWeb.Plug.ApiAuthenticatedTest do
   use WingmanFinderWeb.ConnCase
 
   describe "init/1" do
-    test "assert false", %{conn: _conn} do
+    test "it returns the opts passed", %{conn: _conn} do
       assert false
     end
   end
 
   describe "call/2" do
-    test "assert false", %{conn: _conn} do
+    test "validates the an app/user pair based on the authorization header", %{conn: _conn} do
+      assert false
+    end
+
+    test "it gets an invalid authorization header", %{conn: _conn} do
+      assert false
+    end
+
+    test "the call to Session.validate_access_token fails", %{conn: _conn} do
+      assert false
+    end
+
+    test "the call to Authentication.get_app fails", %{conn: _conn} do
+      assert false
+    end
+
+    test "the call to Authentication.get_user fails", %{conn: _conn} do
       assert false
     end
   end
