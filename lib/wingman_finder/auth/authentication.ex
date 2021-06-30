a3178477a7f2f29d73882732f9477438e69aeb4a
Robby Thompson <robsdudeson@gmail.com>
2021-06-30 16:13:07 -0400

    authentication is [mostly] tested

--- lib/wingman_finder/auth/authentication.ex
+++ lib/wingman_finder/auth/authentication.ex
@@ -2,37 +2,29 @@ defmodule WingmanFinder.Auth.Authentication do
   @moduledoc """
   deals with user authentication into the system
   """
-  alias Ecto.Changeset
+  import Inject, only: [i: 1]
 
   alias WingmanFinder.{App, Repo, User}
 
-  @spec get_app(map()) :: {:ok, App.t()} | {:error, Changeset.t()}
-  def get_app(%{"client_id" => client_id, "client_secret" => client_secret}),
-    do: Repo.get_by(App, client_id: client_id, client_secret: client_secret)
+  @spec get_app(list()) :: App.t() | nil
+  def get_app(opts), do: i(Repo).get_by(App, opts)
 
-  @spec get_app(integer()) :: {:ok, App.t()} | {:error, Changeset.t()}
-  def get_app(id) when is_integer(id),
-    do: Repo.get(App, id)
-
-  @spec get_user(integer() | map()) :: {:ok, User.t()} | {:error, Changeset.t()}
-  def get_user(id) when is_integer(id),
-    do: Repo.get(User, id)
-
-  def get_user(%{"username" => username}), do: Repo.get_by(User, username: username)
+  @spec get_user(list()) :: User.t() | nil
+  def get_user(opts), do: i(Repo).get_by(User, opts)
 
   @doc """
   fetches a user based on username and verifies that the password given matched the hash
   """
-  @spec verify_user(params :: map()) :: {:ok, User.t()} | {:error, String.t()}
-  def verify_user(%{"password" => password} = params) do
-    params
+  @spec verify_user(String.t(), String.t()) :: {:ok, map()} | {:error, String.t()}
+  def verify_user(username, password) do
+    [username: username]
     |> get_user()
-    |> Bcrypt.check_pass(password)
+    |> i(Bcrypt).check_pass(password)
   end
 
   @doc """
   hashes the given password using bcrypt
   """
   @spec hash_password(String.t()) :: %{password_hash: String.t()}
-  def hash_password(password), do: Bcrypt.add_hash(password)
+  def hash_password(password), do: i(Bcrypt).add_hash(password)
 end
--- test/wingman_finder/auth/authentication_test.exs
+++ test/wingman_finder/auth/authentication_test.exs
@@ -1,21 +1,117 @@
 defmodule WingmanFinder.Auth.AuthenticationTest do
   use WingmanFinder.DataCase
 
-  describe "get_app/2" do
-    test "assert false" do
-      assert false
+  import Double, only: [stub: 3]
+  import Inject, only: [register: 2]
+
+  import WingmanFinder.Factory
+
+  alias WingmanFinder.Auth.Authentication
+  alias WingmanFinder.{App, Repo, User}
+
+  @get_by_opts [my: :opts]
+
+  @password "keep_it_secret"
+  @password_hash "cornbeef"
+
+  describe "get_app/1" do
+    setup [:setup_repo_stub]
+
+    test "it wraps Repo.get_by/2 and returns an App" do
+      assert %App{} = Authentication.get_app(@get_by_opts)
+
+      assert_receive {Repo, :get_by, [App, @get_by_opts]}
+    end
+  end
+
+  describe "get_user/1" do
+    setup [:setup_repo_stub]
+
+    test "it wraps Repo.get_by/2 and returns a User" do
+      assert %User{} = Authentication.get_user(@get_by_opts)
+
+      assert_receive {Repo, :get_by, [User, @get_by_opts]}
     end
   end
 
   describe "verify_user/2" do
-    test "assert false" do
-      assert false
+    setup [:setup_bcrypt_stub]
+
+    setup do
+      [user: insert(:user)]
+    end
+
+    test "it fetches the user and verifies the password via Bcrypt", %{
+      user:
+        %{
+          id: expected_user_id,
+          password_hash: expected_password_hash,
+          password: expected_password
+        } = user
+    } do
+      assert {:ok, %User{id: ^expected_user_id}} =
+               Authentication.verify_user(user.username, user.password)
+
+      assert_receive {Bcrypt, :check_pass,
+                      [
+                        %User{id: actual_user_id, password_hash: actual_password_hash},
+                        ^expected_password
+                      ]}
+
+      assert expected_user_id == actual_user_id
+      assert expected_password_hash == actual_password_hash
+    end
+
+    test "User does not exist", %{user: %{password: expected_password} = user} do
+      Repo.delete!(User, user)
+
+      assert {:error, "blew up"} = Authentication.verify_user(user.username, user.password)
+
+      assert_receive {Bcrypt, :check_pass, [nil, ^expected_password]}
+    end
+
+    @tag check_pass_result: {:error, "blew up"}
+    test "Bcrypt fails password check", %{
+      user: %{id: user_id, password: expected_password} = user
+    } do
+      assert {:error, "blew up"} = Authentication.verify_user(user.username, user.password)
+
+      assert_receive {Bcrypt, :check_pass,
+                      [
+                        %User{id: ^user_id},
+                        ^expected_password
+                      ]}
     end
   end
 
-  describe "hash_password/2" do
-    test "assert false" do
-      assert false
+  describe "hash_password/1" do
+    setup [:setup_bcrypt_stub]
+
+    test "calls Bcrypt to hash the password" do
+      assert %{password_hash: @password_hash} = Authentication.hash_password(@password)
+      assert_receive {Bcrypt, :add_hash, [@password]}
     end
+  end
+
+  defp setup_bcrypt_stub(context) do
+    stub =
+      Bcrypt
+      |> stub(:add_hash, fn _password -> %{password_hash: @password_hash} end)
+      |> stub(:check_pass, fn struct, _password ->
+        if context[:check_pass_result], do: context[:check_pass_result], else: {:ok, struct}
+      end)
+
+    register(Bcrypt, stub)
+    []
+  end
+
+  defp setup_repo_stub(_context) do
+    stub =
+      Repo
+      |> stub(:get_by, fn App, _opts -> insert(:app) end)
+      |> stub(:get_by, fn User, _opts -> insert(:user) end)
+
+    register(Repo, stub)
+    []
   end
 end
